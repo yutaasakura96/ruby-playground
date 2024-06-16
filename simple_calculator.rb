39c30f807f55809c1c5d826fdee9437ffb536678
Yuta Asakura <yuta.asakura.se@gmail.com>
2024-06-16 22:11:53 +0900

    simple calculator

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -19,11 +19,34 @@ def mod (first_number, second_number)
 end
 
 
-puts 'simple calculator'
-puts 'enter first number'
+puts 'Simple Calculator'
+puts 'Enter first number'
 num1 = gets.chomp.to_f
-puts  'enter second number'
+puts  'Enter second number'
 num2 = gets.chomp.to_f
+puts 'What do you want to do?'
+puts 'Enter 1 for multiplication, 2 for division, 3 for addition, 4 for subtraction, 5 for modulo'
+user_entry = gets.chomp.to_f
+
+if user_entry == 1
+  puts "You have chosen multiplication"
+  puts "#{multiply(num1,num2)}"
+elsif user_entry == 2
+  puts 'You have chosen division'
+  puts "#{divide(num1,num2)}"
+elsif user_entry == 3
+  puts 'You have chosen addition'
+  puts "#{add(num1,num2)}"
+elsif user_entry == 4
+  puts 'You have chosen subtraction'
+  puts "#{subtract(num1,num2)}"
+elsif user_entry == 5
+  puts 'You have chosen modulo'
+  puts "#{mod(num1,num2)}"
+else
+  puts "Invalid entry"
+end
+
 puts "the first number multiplied by the second number is #{multiply(num1,num2)}"
 puts "the first number divided by the second number is #{divide(num1,num2)}"
 puts "the first number added to the second number is #{add(num1,num2)}"
