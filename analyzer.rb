792df51b43a7c268878ab19e0cb6b6914af4b073
Yuta Asakura <yuta.asakura.se@gmail.com>
2024-06-16 19:57:16 +0900

    analyzer homework

---
+++ analyzer.rb
@@ -0,0 +1,7 @@
+puts 'Enter your first name'
+first_name = gets.chomp
+puts 'Enter your last name'
+last_name = gets.chomp
+puts "Your full name is #{first_name} #{last_name}"
+puts "Your full name reversed is #{last_name.reverse} #{first_name.reverse}"
+puts "yout full name has #{first_name.length + last_name.length} characters in it"
