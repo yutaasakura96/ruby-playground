e4e3441e05456745501ad5e697a6fa287c4d64c4
Yuta Asakura <yuta.asakura.se@gmail.com>
2024-06-17 14:11:14 +0900

    wrapping up Authenticator project

--- authenticator.rb
+++ authenticator.rb
@@ -1,3 +1,5 @@
+# users array where username and password are stored
+
 users = [
   {username: 'yuta', password: 'password1'},
   {username: 'kenta', password: 'password2'},
@@ -6,30 +8,34 @@ users = [
   {username: 'eri', password: 'password5'},
 ]
 
+# authentication method to check and verify if username/password combination exists
 def auth_user(username, password, list_of_users)
-  users.each do |user|
-    if user[:username] == username && user[:password]
-      puts user
-      break
-    else
-      puts 'Credentials were not correct'
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && user_record[:password] == password
+      return user_record
     end
   end
+  "Credentials were not correct"
 end
-
-  puts 'Welcome to the authenticator'
-  25.times { print "-"}
-  puts "This program will take input from the user and compare password"
-  puts "If password is correct, you will get back user object"
-
-  attempts = 1
-  while attempts < 4
-    print "Username: "
-    username = gets.chomp
-    print "Password: "
-    password = gets.chomp
-    puts "Press n to quit or any other key to continue"
-    input = gets.chomp.downcase
-   break if input == "n"
-    attempts += 1
-  end
+ 
+# program execution flow
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+ 
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 4
